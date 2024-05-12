f923d6f3dce17b4e56656c59e056e06ceea0f2bc
Ludovico M. Spitaleri <ludovico.spitaleri@gmail.com>
2024-05-12 17:27:17 +0200

    elaborato 1 finished

--- Elaborati/Elaborato 1/Elaborato 1/main.c
+++ Elaborati/Elaborato 1/Elaborato 1/main.c
@@ -22,44 +22,62 @@ void main() {
 
 	__asm
 	{
-		JMP _Main;					//	jump to Main()
+									//	Reset registers
+		XOR EAX, EAX;				//	EAX = 0
+		XOR EBX, EBX;				//	EBX = 0
+		XOR EDX, EDX;				//	ECX = 0
+		XOR EDX, EDX;				//	EDX = 0
 
-	_Swap:							//	Swap():
-
-	_Confronto:						//	Confronto():
-		XOR ECX, ECX;				//		ECX = 0
+		MOV EAX, num;				//	EAX = num
+		CMP EAX, 1;					//	compare EAX, 1
+		JA _Sort;					//	if EAX > 1: Sort()
+		JMP _End;					//	else: END
 	
+									//	Confronto: Finchè non finisce la prima stringa, cicla carattere per carattere
+									//		- Se sono uguali, continua a ciclare
+									//		- Se sono diversi, fai la differenza (aggiornando i flag) e ritorna
+									// 
+	_Confronto:						//	Confronto(ESI=word1, EDI=word2):
+		XOR ECX, ECX;				//		ECX = 0
+									
 	_CicloStringa:					//		do:
 		MOV DL, [ESI][ECX];			//			DL = word1[ECX]
-		CMP DL, 0;					//			if DL == '\0':
-		JZ _CicloStringaEnd;		//				jump to CicloStringaEnd
-
 		MOV DH, [EDI][ECX];			//			DH = word2[ECX]
+		INC ECX;					//			ECX++ (updated here to not interfere with flags after compares)
 
-	_CicloStringaEnd:				//	CicloStringaEnd:
-		CMP DL, DH;					//		do DL-DH, update flags
-		RET;
+		CMP DL, DH;					//			compare DL DH
+		JNE _ConfrontoRet;			//			if DL != DH: return
+		CMP DL, 0;					//			compare DL, 0
+		JE _ConfrontoRet;			//			if DL (== DH) == 0: return
+		
+		JMP _CicloStringa;			//		while (true)
 
-	_Main:							//	Main():
-		XOR EAX, EAX;				//		EAX = 0
-		XOR EBX, EBX;				//		EBX = 0
-		XOR EDX, EDX;				//		EDX = 0
+	_ConfrontoRet:
+		RET;						// 
 
-		MOV EAX, num;				//		EAX = num (iterazioni)
+	_Sort:							//	Sort(EAX=num):
 	_Ciclo_Iterazioni:				//		do:
-		MOV EBX, num;				//			EBX = num (confronti)
-		DEC EBX;					//			EBX-- (per andare da n-1 a 1)
+		MOV EBX, num;				//			EBX = num
+		DEC EBX;					//			EBX-- (num-1..1)
 	
 	_Ciclo_Confronti:				//			do:
-		MOV ESI, strings[EBX*4];	//				ESI = (int)strings[EBX]	  (word1: strings[n-1..1])
-		MOV EDI, strings[EBX*4-4];	//				ESI = (int)strings[EBX-1] (word2: strings[n-2..0])
-		CALL _Confronto;			//				Confronto()
+		MOV ESI, strings[EBX*4-4];	//				ESI = (int)strings[EBX-1] (word1: strings[num-2..0])
+		MOV EDI, strings[EBX*4];	//				EDI = (int)strings[EBX]	  (word2: strings[num-1..1])
 		
+		CALL _Confronto;			//				Confronto()
+		JBE _NoSwap;				//				if word1 <= word2: skip swap
+									//				Swap:
+		MOV strings[EBX*4-4], EDI;	//				strings[num-2..0] = word2
+		MOV strings[EBX*4], ESI;	//				strings[num-1..1] = word1
+
+	_NoSwap:					
 		DEC EBX;					//				EBX--
 		JNZ _Ciclo_Confronti;		//			while EBX > 0
 		
 		DEC EAX;					//			EAX--
 		JNZ _Ciclo_Iterazioni;		//		while EAX > 0
+
+	_End:
 	}
 
 	// Stampa su video
